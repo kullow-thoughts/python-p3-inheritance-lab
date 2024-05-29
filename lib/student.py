9503df4bf25889b2cafdeaf98ccb2213223af1cc
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-05-29 12:08:09 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name,last_name)
+        knowledge=[]
+        self.knowledge = knowledge
+
+    def learn(self,new_info):
+        self.knowledge=new_info
